19c9e6bdefc21c56f396c66a829747012e225fd5
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-17 11:54:43 -0400

    using state to update the payer value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 export default function Player() {
+ const [name,setName] = useState('');
+ const [isediting,setIsEditing] = useState(false);
+
+ function handleName(event){
+  setName(event.target.value)
+ }
+
+ function handleClick(){
+  setIsEditing((editing) => !editing)
+ }
+ let buttonCaption = 'Set Name';
+ if(isediting){
+  buttonCaption = 'Edit'
+ }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isediting? name : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={name} onChange={handleName}/>
+        <button onClick={handleClick}>{buttonCaption}</button>
       </p>
     </section>
   );
