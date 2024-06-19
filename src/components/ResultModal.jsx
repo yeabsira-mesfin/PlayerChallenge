150f90a72f404dfe21beae8609941c7442668e08
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-19 08:33:13 -0400

    create portal and finalized app

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,29 +1,38 @@
-import React,{forwardRef,useImperativeHandle,useRef} from 'react'
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-const ResultModal = forwardRef(function ResultModal ({targetTime, remainingTime, onReset},ref) {
- const dialog =  useRef();
-const score = Math.round((1- remainingTime / (targetTime * 1000)) * 100);
- 
-  useImperativeHandle(ref,()=> {
+  useImperativeHandle(ref, () => {
     return {
-      open(){
+      open() {
         dialog.current.showModal();
-      }
-    }
+      },
+    };
   });
-  return (
-    <dialog ref={dialog} className='result-modal' >
-        <h2>
-            
-            { remainingTime > 0 ? `Your score: ${score}`: ' '}
-        </h2>
-        <p>The target time was <b>{targetTime} second {targetTime>1? 's': ''}</b></p>
-        <p>You stopped the timer with <b>{(remainingTime/1000).toFixed(2)} seconds left.</b></p>
-        <form action="dialog" onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>
-  )
-})
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      <h2>{remainingTime > 0 ? `Your score: ${score}` : " "}</h2>
+      <p>
+        The target time was{" "}
+        <b>
+          {targetTime} second {targetTime > 1 ? "s" : ""}
+        </b>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <b>{(remainingTime / 1000).toFixed(2)} seconds left.</b>
+      </p>
+      <form action="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
 
-export default ResultModal
+export default ResultModal;
